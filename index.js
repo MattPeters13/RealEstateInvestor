116fd2b20b9083a210bd979e44ecee46de4e1690
MattPeters13 <mapeters@csumb.edu>
2023-04-27 09:10:50 -0700

    Initial foundation
    Added all necessary code to start building the site.

---
+++ index.js
@@ -0,0 +1,17 @@
+// Require necessary modules
+const express = require('express');
+
+// Create an instance of express application
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+// Define routes
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
