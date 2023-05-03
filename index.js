87e9de35a698ad4ac752ceb352be8ab066fb2d57
Irvine <81997674+1rvine@users.noreply.github.com>
2023-05-03 12:32:11 -0700

    home ui

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const express = require('express');
 
 // Create an instance of express application
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
 // Define routes
 app.get('/', (req, res) => {
-  res.render('caprate');
+	res.render('caprate');
+});
+
+app.get('/home', (req, res) => {
+	res.render('home');
 });
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+	console.log(`Server listening on port ${PORT}`);
 });
